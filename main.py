9bfb19a7a73a27533af5f773b18fa97bfa603111
Jett Hollister <jett.hollister@duke.edu>
2024-02-19 09:28:25 -0800

    Added starter script to main

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
+import os
+
+from atproto import Client
+from dotenv import load_dotenv
+
+# Load environment variables
+load_dotenv()
+
+# Bluesky credentials
+BLUESKY_USERNAME = os.getenv("BLUESKY_USERNAME")
+BLUESKY_PASSWORD = os.getenv("BLUESKY_PASSWORD")
+
+# Create a Bluesky client
+client = Client("https://bsky.social")
+
+
 def main():
-    return
+    client.login(BLUESKY_USERNAME, BLUESKY_PASSWORD)
+    client.post("🙂")
 
 
 if __name__ == "__main__":
